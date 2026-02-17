6023c3e764386d8a011d2684de42ef14511dab3f
Michaela P. <ponistovam92@gmail.com>
2026-02-17 21:16:18 +0100

    výpočet celkobých prodejů a průměrné ceny v kategoriích

--- projekt1.py
+++ projekt1.py
@@ -1,4 +1,5 @@
 obrat_kategorie = dict()
+prodej_celkem = dict()
 
 with open ("prodeje.txt", "r", encoding="utf-8") as file:
     for line in file:
@@ -14,6 +15,7 @@ with open ("prodeje.txt", "r", encoding="utf-8") as file:
             obrat = prodane_mn * cena
             
             obrat_kategorie[kategorie] = obrat_kategorie.get(kategorie, 0) + obrat
+            prodej_celkem[kategorie] = prodej_celkem.get(kategorie, 0) + prodane_mn
             
         except:
             print(f'Přeskakuji chybný záznam {line}')
@@ -22,3 +24,15 @@ with open ("prodeje.txt", "r", encoding="utf-8") as file:
 print("\n-- PŘEHLED TRŽEB --")
 for kategorie, celkovy_obrat in obrat_kategorie.items():
     print(f"{kategorie:10}: {celkovy_obrat} Kč")
+
+print("\n-- CELKEM PRODÁNO KUSŮ --") 
+for kategorie, prodano in prodej_celkem.items():
+    print(f"{kategorie:10}: {prodano} ks")
+    
+print("\n-- PRŮMĚRNÁ PRODEJNÍ CENA V KATEGORIÍCH --") 
+for kategorie in obrat_kategorie:
+    trzby = obrat_kategorie[kategorie]
+    kusy = prodej_celkem[kategorie]
+    
+    prumer = trzby / kusy
+    print(f"{kategorie:10}: {prumer: .2f} Kč")
